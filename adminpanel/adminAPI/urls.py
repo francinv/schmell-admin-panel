9b3b28f331d8bbb6567f9beda831a5e99bbfeaba
Francin Anoj Vincent <francinv@stud.ntnu.no>
2021-12-24 00:39:02 +0100

    (#3) Deleted unwanted module/APP

--- adminpanel/adminAPI/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers, urlpatterns
-from .api import UserViewSet, GameViewSet, QuestionViewSet
-
-router = routers.DefaultRouter()
-router.register('game', GameViewSet, 'weekgame')
-router.register('game_question', QuestionViewSet, 'question')
-router.register('user', UserViewSet, 'users')
-
-urlpatterns = router.urls
