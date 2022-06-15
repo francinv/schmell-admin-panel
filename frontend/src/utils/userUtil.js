37f0ac2cf4e580fabf82ddb972f64a5f966fc640
Francin Anoj Vincent <francinv@stud.ntnu.no>
2022-06-15 22:13:39 +0200

    Fixed staging bug. Not updated urls correctly, was pointing to localhost

--- frontend/src/utils/userUtil.js
+++ frontend/src/utils/userUtil.js
@@ -3,7 +3,7 @@ import axiosService from "../services/axiosService";
 export const checkIfUserIsLoggedIn = async () => {
     const access = localStorage.getItem('access');
     if (access) {
-        const res = await axiosService.post('http://localhost:8000/api/auth/key/validate/', {token: access}).then(res => res.status);
+        const res = await axiosService.post('api/auth/key/validate/', {token: access}).then(res => res.status);
         if (res === 200) {
             return true;
         } else {
